72e4afc160721a310bec498c000c33fc92f8221a
Kevin Wang <kevin.wang19@yahoo.com>
2020-09-07 12:57:49 -0400

    Initial Commit

---
+++ src/sportsmania/game/Net.java
@@ -0,0 +1,23 @@
+package sportsmania.game;
+
+
+public class Net extends GameObject{
+    
+    /**
+     * Constructed a Net object whose upper-left corner is specified as (x,y) 
+     * and whose width and height are specified by the arguments of the same name 
+     * @param x
+     * @param y
+     * @param width
+     * @param height
+     * @param img
+     */
+    public Net(final int x, final int y, final int width, final int height, final String img){
+        super(x, y, height, width, img);
+    }
+
+    @Override
+    public void update(final Game game) {
+    }
+    
+}
